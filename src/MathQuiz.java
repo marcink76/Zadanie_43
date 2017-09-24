69ec971f72908bfe72cf1dbef66aa2f856b752ca
Marcin Konieczny <marcin_k@coni.pl>
2017-09-24 20:35:15 +0200

    Version 2.1 z klasą losującą i scannerem statycznym

--- src/MathQuiz.java
+++ src/MathQuiz.java
@@ -1,45 +1,45 @@
 import java.util.Scanner;
 
 public class MathQuiz {
+    private static Scanner answerInput = new Scanner(System.in);
 
     public static boolean question1(){
-        Scanner answerInput = new Scanner(System.in);
+
         boolean answer;
 
         int factor1 = Randomize.randomize(10);
         int factor2 = Randomize.randomize(10);
 
-        System.out.println("Jaki jest wynik mnożenia to: " +factor1 +" x "+factor2 + "?");
+        System.out.println("Jaki jest wynik mnożenia to: " + factor1 +" x " + factor2 + "?");
 
         int solution = answerInput.nextInt();
             if (solution == (factor1*factor2)){
                 System.out.println("Dobrze!");
                 answer = true;
             }else{
-                System.out.println("Zle! Prawidłowy wynik to: " + factor1 * factor2 + "");
+                System.out.println("Zle! Prawidłowy wynik to: " + factor1 * factor2 + "!");
                 answer = false;
             }
         return answer;
     }
     public static boolean question2(){
 
-        Scanner answerInput = new Scanner(System.in);
         boolean answer;
 
         int squareSide = Randomize.randomize(10);
-        System.out.println("Jakie jest pole kwadratu o boku "+squareSide+"?");
+        System.out.println("Jakie jest pole kwadratu o boku " + squareSide + "?");
         int solution = answerInput.nextInt();
         if (solution == Math.pow(squareSide,2)){
             System.out.println("Dobrze!");
             answer = true;
         }else{
-            System.out.println("Zle prawidłowy wynik to: "+Math.pow(squareSide,2)+"");
+            System.out.println("Zle prawidłowy wynik to: "+Math.pow(squareSide,2)+"!");
             answer = false;
         }
         return answer;
     }
     public static boolean question3(){
-        Scanner answerInput = new Scanner(System.in);
+
         boolean answer;
         int square = Randomize.randomize(30);
         System.out.println("Jaki jest pierwiastek kwadratowy z liczby: "+square*square+"?");
@@ -49,7 +49,7 @@ public class MathQuiz {
             System.out.println("Dobrze!");
             answer = true;
         }else{
-            System.out.println("Zle prawidłowy wynik to: "+square+"");
+            System.out.println("Zle prawidłowy wynik to: "+square+"!");
 
             answer = false;
         }
